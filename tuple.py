08d13a9460f2ebc05d747ca7842ac8458e9a3088
Meghana Sri Konakalla <konakallameghana@gmail.com>
2026-03-30 18:38:13 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,15 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #tuple
+>>> a=(5,8.9,"python",4+9j,True,False)
+>>> type(a)
+<class 'tuple'>
+>>> #len
+>>> len(a)
+6
+>>> #count
+>>> a.count(True)
+1
+>>> #index
+>>> a.index("python")
+2
